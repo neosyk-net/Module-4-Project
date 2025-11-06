586376b39603f592d5ff14ab0931a2fdf3295f6d
neosyk-net <runerdycastillo@gmail.com>
2025-11-05 23:12:18 -0500

    Good Update!

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-const url = 'http://www.omdbapi.com/?i=tt3896198&apikey=82451f49';
+// const url = 'http://www.omdbapi.com/?i=tt3896198&apikey=82451f49';
 
-async function fetchMovieData() {
-const response = await fetch(url);
-const data = await response.json();
-console.log(data);
+// async function fetchMovieData() {
+// const movies = await fetch(url);
+// const moviesData = await movies.json();
+// console.log(moviesData);
+// }
+
+
+// fetchMovieData();
+
+const url = 'https://imdb236.p.rapidapi.com/api/imdb/cast/nm0000190/titles';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '3fc5582ab1mshabbdc05d6787823p1eb946jsn085ff5d111ec',
+		'x-rapidapi-host': 'imdb236.p.rapidapi.com'
+	}
+};
+
+
+async function getData() {
+ try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
 }
 
-fetchMovieData();
+getData();
